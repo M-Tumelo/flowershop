8a6f785fa69240bb3c9ee8ea68ae665249e260c9
Tumelo <g17m9389@campus.ru.ac.za>
2019-02-23 23:57:18 +0200

    Mock Tests

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -11,20 +11,39 @@ namespace Tests
         {
         }
 
+       // [Test]
+        //public void Test1()
+        //{
+        //    //Arrange
+        //    IOrderDAO orderDAO = Substitute.For<IOrderDAO>();
+        //    IClient client = Substitute.For<IClient>();
+        //    IOrder Order = Substitute.For<IOrder>();
+        //    Order order = new Order(orderDAO, client);
+
+        //    //Act
+        //    order.Deliver();
+
+        //    //Assert
+        //    orderDAO.Received().SetDelivered(Arg.Any<IOrder>());
+        //}
         [Test]
-        public void Test1()
+        public void Test2()
         {
             //Arrange
-            IOrderDAO orderDAO = Substitute.For<IOrderDAO>();
-            IClient client = Substitute.For<IClient>();
-            IOrder Order = Substitute.For<IOrder>();
-            Order order = new Order(orderDAO, client);
+            //IOrderDAO orderDAO = Substitute.For<IOrderDAO>();
+            IFlowerDAO flowerDAO = Substitute.For<IFlowerDAO>();
+            //IClient client = Substitute.For<IClient>();
+            //IOrder Order = Substitute.For<IOrder>();
+            Order order = Substitute.For<Order>(Arg.Any<IOrderDAO>(), Arg.Any<IClient>());
+            var flower = Substitute.For<Flower>(flowerDAO,"Red",274.99,3);
+            var flower1 = Substitute.For<Flower>(flowerDAO, "White Roses", 129.99, 10);
+            var flower2 = Substitute.For<Flower>(flowerDAO, "SunFlower", 50, 30);
 
             //Act
-            order.Deliver();
+            order.AddFlowers(Arg.Any<Flower>(),Arg.Any<int>());
 
             //Assert
-            orderDAO.Received().SetDelivered(Arg.Any<IOrder>());
+            Assert.That(order.Price, Is.EqualTo(2));
         }
     }
 }
