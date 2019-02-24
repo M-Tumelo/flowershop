3d72eb47ebb7cd1ddf7933a50ac4287f6ebf7012
Tumelo <g17m9389@campus.ru.ac.za>
2019-02-24 11:15:36 +0200

    Tests to check price method

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -11,39 +11,39 @@ namespace Tests
         {
         }
 
-       // [Test]
-        //public void Test1()
-        //{
-        //    //Arrange
-        //    IOrderDAO orderDAO = Substitute.For<IOrderDAO>();
-        //    IClient client = Substitute.For<IClient>();
-        //    IOrder Order = Substitute.For<IOrder>();
-        //    Order order = new Order(orderDAO, client);
-
-        //    //Act
-        //    order.Deliver();
-
-        //    //Assert
-        //    orderDAO.Received().SetDelivered(Arg.Any<IOrder>());
-        //}
         [Test]
+       /* public void Test1()
+        {
+            //Arrange
+            IOrderDAO orderDAO = Substitute.For<IOrderDAO>();
+            IClient client = Substitute.For<IClient>();
+            IOrder Order = Substitute.For<IOrder>();
+            Order order = new Order(orderDAO, client);
+
+            //Act
+            order.Deliver();
+
+            //Assert
+            orderDAO.Received().SetDelivered(Arg.Any<IOrder>());
+        }
+        */
         public void Test2()
         {
             //Arrange
             //IOrderDAO orderDAO = Substitute.For<IOrderDAO>();
             IFlowerDAO flowerDAO = Substitute.For<IFlowerDAO>();
-            //IClient client = Substitute.For<IClient>();
-            //IOrder Order = Substitute.For<IOrder>();
-            Order order = Substitute.For<Order>(Arg.Any<IOrderDAO>(), Arg.Any<IClient>());
+            IClient client = Substitute.For<IClient>();
+            IOrderDAO Order = Substitute.For<IOrderDAO>();
+            Order order = Substitute.ForPartsOf<Order>(Order, client,false);
             var flower = Substitute.For<Flower>(flowerDAO,"Red",274.99,3);
             var flower1 = Substitute.For<Flower>(flowerDAO, "White Roses", 129.99, 10);
             var flower2 = Substitute.For<Flower>(flowerDAO, "SunFlower", 50, 30);
 
             //Act
-            order.AddFlowers(Arg.Any<Flower>(),Arg.Any<int>());
+            order.Price.Returns(30);
 
             //Assert
-            Assert.That(order.Price, Is.EqualTo(2));
+            Assert.That(order.Price, Is.EqualTo(30));
         }
     }
 }
